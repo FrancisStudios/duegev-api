74000b40507e91f8ee962ba9c89b7afb7b132a82
Francis <vlfmap@gmail.com>
2025-05-22 20:38:14 +0200

    cleaned all old unnecessary stuff

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import process from 'process';
 import cors from 'cors'
-//import DuegevUserEndpoint from './API/user.endpoint.js';
-//import DuegevLabelEndpoint from './API/label.endpoint.js';
 import MessageInterface from './utils/message.interface.js';
 
 const DuegevBackendAPI = express();
@@ -17,11 +15,6 @@ DuegevBackendAPI.use(
 );
 
 console.log('Duegev monolith server started');
-
-/* Init Endpoint Services */
-// DuegevUserEndpoint.initEndpoint(DuegevBackendAPI);
-// DuegevLabelEndpoint.initEndpoint(DuegevBackendAPI);
-
 
 /* ROOT and basic API definitions */
 DuegevBackendAPI.get('/', function (req, res) {
--- src/utils/database/database.config.js
+++
@@ -1,8 +0,0 @@
-const DBCONFIG = {
-    host: 'localhost',
-    user: 'unicumnet',
-    password: 'testdb123',
-    database: 'duegev-wiki'
-}
-
-export default DBCONFIG;
