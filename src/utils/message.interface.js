8f4051fb8ddb88ad58cb7463bfe0f33ba6934555
Francis <vlfmap@gmail.com>
2025-05-24 14:59:11 +0200

    whateve

--- src/utils/message.interface.js
+++ src/utils/message.interface.js
@@ -2,14 +2,15 @@ export default class MessageInterface {
     /**
      * 
      * @param {status_code} status 
-     * @param {string} message 
+     * @param {string} message
+     * @param {boolean} json 
      * @returns 
      */
-    static construct(status, message) {
+    static construct(status, message, json) {
 
         return `{
         "status": "${status}",
-        "message": "${message}"
+        "message": ${message}
         }`;
     }
 
